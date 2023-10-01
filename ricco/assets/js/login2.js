52e5c4e05e87455fe54a3e59035bde8bbd23f5ec
LAURA PATRICIA CRUZ <95201770+LauraPatriciaCruz@users.noreply.github.com>
2023-10-01 03:03:54 -0300

    AGREGUÉ ARCHIVO login2.js, FOTOS, Y QUITÉ EL CHECK MARK DE RECORDAR

--- ricco/assets/js/login2.js
+++ ricco/assets/js/login2.js
@@ -1,8 +1,6 @@
 // ---------------LLAMADO A FUNCIONES----------------
 document.getElementById("in").addEventListener("click", ingresar);
 document.getElementById("re").addEventListener("click", registrar);
-document.getElementById("cualOjo").addEventListener("click", myLogPassword );
-document.getElementById("queOjo").addEventListener("click", myRegPassword);
 
 // ------------CAJA INGRESO Y REGISTRO---------------
 let cajaIngreso = document.getElementById('ingreso');
@@ -20,41 +18,5 @@ function registrar(){
     cajaRegistro.style.right = "25px";
     boton.style.left = "150px";
 }
-
-// -----------------VISTA DE CONTRASEÑA-----------------
-
-function myLogPassword(){
-
-    let passwordIn = document.getElementById('contrasenaIngreso');
-    let ojoCompleto = document.getElementById('ver');
-    let ojoTachado = document.getElementById('ver-slash');
-
-    if(passwordIn.type === "password"){
-        passwordIn.type = "text"
-        ojoCompleto.style.opacity = "0";
-        ojoTachado.style.opacity = "1";
-    }else{    
-        passwordIn.type = "password"
-        ojoCompleto.style.opacity = "1";
-        ojoTachado.style.opacity = "0";
-    }
-}
-
-function myRegPassword(){
-
-    let passwordReg = document.getElementById('contrasenaRegistro');
-    let ojo = document.getElementById("ver-2");
-    let tachado = document.getElementById("ver-slash-2");
-
-    if(passwordReg.type === "password"){
-        passwordReg.type = "text"
-        ojo.style.opacity = "0"; 
-        tachado.style.opacity = "1"; 
-    }else{    
-        passwordReg.type = "password" 
-        ojo.style.opacity = "1"; 
-        tachado.style.opacity = "0"; 
-    }
-}    
 
 
