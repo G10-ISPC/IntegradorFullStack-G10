a863403f166bcf8f615da56176fd84f31fabbacc
LAURA PATRICIA CRUZ <95201770+LauraPatriciaCruz@users.noreply.github.com>
2023-10-12 00:40:38 -0300

    ARREGLÉ DESPERFECTO QUE SURGIÓ AL CAMBIAR EL LAYOUT DE LONGIN

--- ricco/frontend/assets/js/login2.js
+++ ricco/frontend/assets/js/login2.js
@@ -1,25 +1,3 @@
-// ---------------LLAMADO A FUNCIONES----------------
-document.getElementById("in").addEventListener("click", ingresar);
-//document.getElementById("re").addEventListener("click", registrar);
-
-// ------------CAJA INGRESO Y REGISTRO---------------
-let cajaIngreso = document.getElementById('ingreso');
-//let cajaRegistro = document.getElementById('registro');
-let boton = document.getElementById('btn');
-
-function ingresar(){
-    cajaIngreso.style.left = "27px";
-    cajaRegistro.style.right = "-350px";
-    boton.style.left = "0px";
-}
-
-function registrar(){
-    cajaIngreso.style.left = "-350px";
-    cajaRegistro.style.right = "25px";
-    boton.style.left = "150px";
-}
-
-// =================================================================
 // ----VALIDACION DE CORREO ELECTRÓNICO Y CONTRASEÑA EN LOGIN-------
 
 document.addEventListener('DOMContentLoaded', function() {
